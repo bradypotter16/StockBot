fffafc2c2070751f963d7307afed1c52de6a819a
Brady Potter <bradypotter16@gmail.com>
2020-12-18 15:09:48 -0500

    Improved index.js

--- index.js
+++ index.js
@@ -26,27 +26,89 @@ bot.on('ready', () =>
 bot.on('message', msg => 
 {
 
-    if (msg.content == 'ping')
-    {
-
-        msg.channel.send('pong');
-
-    }
-
-    else if (msg.content.startsWith('!show'))
+    if (msg.content.startsWith('!show'))
     {
 
         let splitArray = msg.content.split(" ");
 
-        stockSymbol = splitArray[1];
+        ticker = splitArray[1];
+        stockSymbol = ticker.toUpperCase();
 
-        finnhubClient.companyProfile2({'symbol': stockSymbol}, (error, data, response) => {
-            msg.channel.send(data.name)
-            msg.channel.send(data.ticker)
-            msg.channel.send(data.weburl)
+
+        finnhubClient.companyProfile2({'symbol': stockSymbol}, (error, data, response) => 
+        {
+            
+            let titleString = data.name + " (" + data.ticker + ")"
+
+            var infoEmbed = new Discord.MessageEmbed()
+                .setColor('#2832C2')
+                .setThumbnail(data.logo)
+                .setTitle(titleString)
+                .addFields
+                (
+
+                    {name: "Website", value: data.weburl},
+                    {name: "Phone", value: data.phone},
+
+
+                )
+
+                msg.channel.send({embed: infoEmbed});
+        
         });
+            
+        setTimeout(function()
+        {
+        
+            finnhubClient.quote(stockSymbol, (error, data, response) =>
+            {
+
+                let 
+
+                var stockEmbed = new Discord.MessageEmbed()
+                    .setColor('#2832C2')
+                    .addFields
+                    (
+
+                        {name: "Current Value", value: data.c.toFixed(2)},
+                        {name: "Day Open", value: data.o.toFixed(2), inline: true},
+                        {name: "Day High", value: data.h.toFixed(2), inline: true},
+                        {name: "Day Low", value: data.l.toFixed(2), inline: true},
+
+                    );
+
+                    msg.channel.send({embed: stockEmbed});
+ 
+            });
+
+        }, 200)
+
+        setTimeout(function()
+        {
+        
+            finnhubClient.quote(stockSymbol, (error, data, response) =>
+            {
+
+                let 
+
+                var stockEmbed = new Discord.MessageEmbed()
+                    .setColor('#2832C2')
+                    .addFields
+                    (
+
+                        {name: "Current Value", value: data.c.toFixed(2)},
+                        {name: "Day Open", value: data.o.toFixed(2), inline: true},
+                        {name: "Day High", value: data.h.toFixed(2), inline: true},
+                        {name: "Day Low", value: data.l.toFixed(2), inline: true},
+
+                    );
+
+                    msg.channel.send({embed: stockEmbed});
+ 
+            });
+
+        }, 200)
 
     }
 
 });
-//test
