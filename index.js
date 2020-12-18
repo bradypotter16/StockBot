5f9262c885da95518e9c51ac0faa93cab2f05f73
Brady Potter <bradypotter16@gmail.com>
2020-12-18 04:01:24 -0500

    Connected to API and Started the !show function

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const request = require('request');
+var finnhub = require('finnhub');
+var express = require('express');
+var bodyParser = require('body-parser')
 
 const TOKEN = process.env.ACC_TOKEN;
 
-console.log(process.env.ACC_TOKEN);
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = "bve5p2n48v6qmf0h32pg" // Replace this
+const finnhubClient = new finnhub.DefaultApi()
 
 bot.login(TOKEN);
 
@@ -23,14 +30,21 @@ bot.on('message', msg =>
     {
 
         msg.channel.send('pong');
-        console.log();
 
     }
 
-    else if (msg.content.startsWith('!bestStock'))
+    else if (msg.content.startsWith('!show'))
     {
 
-        msg.channel.send('Air Canada');
+        let splitArray = msg.content.split(" ");
+
+        stockSymbol = splitArray[1];
+
+        finnhubClient.companyProfile2({'symbol': stockSymbol}, (error, data, response) => {
+            msg.channel.send(data.name)
+            msg.channel.send(data.ticker)
+            msg.channel.send(data.weburl)
+        });
 
     }
 
